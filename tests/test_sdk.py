f06681f2203b78aea78ee2ead83c19e69a903f55
unknown <sanghyunlee@webzen.com>
2026-05-12 18:49:16 +0900

    test

--- tests/test_sdk.py
+++ tests/test_sdk.py
@@ -7,7 +7,7 @@ import pytest, time
 from conftest import DEVICES
 
 
-#1
+#2
 @pytest.mark.parametrize("driver", DEVICES, indirect=True)
 def test_init(driver):
     device_id = driver.capabilities["udid"]
